8624b241bc4e96da5efa4948319ae6c231849128
Lsm2k <50469406+lucasmoore2k@users.noreply.github.com>
2020-08-09 12:01:02 -0300

    Add files via upload
    Calculadora básica de dois valores

---
+++ Calculadora Básica/Calculadora.php
@@ -0,0 +1,48 @@
+<?php
+
+
+Class Calculadora{
+	private $total;
+	private $numero1;
+	private $numero2;
+
+
+	function __construct(){
+		//Esse construtor os valores vão iniciar com zero
+
+		$this->total = 0;
+		$this->numero = 0;
+		$this->numero = 0;
+	}
+
+	//getters and setters
+	public function setNumero1($parametro_numero1){
+		$this->numero1 = $parametro_numero1;
+
+	}
+	public function setNumero2($parametro_numero2){
+		$this->numero2 = $parametro_numero2;
+
+	}
+
+	public function getTotal(){
+		return $this->total;
+	}
+
+	//Operacoes
+	public function somar(){
+		$this->total = $this->numero1 + $this->numero2;
+	}
+	public function subtrair(){
+		$this->total = $this->numero1 - $this->numero2;
+	}
+	public function multiplicar(){
+		$this->total = $this->numero1 * $this->numero2;
+	}
+	public function dividir(){
+		$this->total = $this->numero1 / $this->numero2;
+	}
+
+}
+
+?>
